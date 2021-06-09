0c8f6ef8dc2c61c72322ea4ce1aa73d3e32d721e
sters <sters@users.noreply.github.com>
2021-06-09 16:46:27 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# github-actions-release-reminder
+# github-actions-collect-unreleased-commits
+
+## Inputs
+
+|Value|Description|
+|---|---|
+|workspace|Collect git log for this workspace path.|
+|mode|Collect method. Details are below.|
+|format|Message format at each commits. Details are below.|
+
+## Supported `mode`
+
+|Mode              |Description                               |
+|------------------|------------------------------------------|
+|only-pull-requests|Includes only `#xx` style commit messages.|
+|only-merge-commit |Includes only `Merge` commit.             |
+|all-commit        |Includes all commit.                      |
+
+## Supported `format`
+
+`format` supports this syntax like template literals.
+
+```
+${subject}
+```
+
+|Values|Description|
+|---|---|
+|subject|commit message|
+|authorName|commit's author name|
+|hash|commit hash|
+|abbrevHash|shorthand commit hash number|
---
+++ index.js
@@ -0,0 +1,110 @@
+// @ts-nocheck
+import { getInput, setFailed, setOutput } from '@actions/core';
+import { execFile } from 'child_process';
+import { Readable } from 'stream';
+import through from 'through2';
+
+const modes = {
+  onlyPullRequests: 'only-pull-requests',
+  onlyMergeCommit: 'only-merge-commit',
+  allCommit: 'all-commit',
+};
+
+const parseInputs = () => {
+  return {
+    workspace: getInput('workspace'),
+    mode: getInput('mode'),
+    format: getInput('format'),
+  };
+};
+
+const run = async (cmd, args, cwd) => {
+  const readable = new Readable();
+  readable._read = () => { };
+
+  let isError = false;
+
+  const child = execFile(cmd, args, {
+    cwd: cwd,
+    maxBuffer: Infinity
+  });
+
+  child.stdout
+    .pipe(through((chunk, enc, cb) => {
+      readable.push(chunk);
+      isError = false;
+      cb();
+    }, (cb) => {
+      setImmediate(() => {
+        if (!isError) {
+          readable.push(null);
+          readable.emit('close');
+        }
+        cb();
+      })
+    }));
+
+  child.stderr
+    .pipe(through.obj((chunk) => {
+      isError = true;
+      readable.emit('error', new Error(chunk));
+      readable.emit('close');
+    }));
+
+  let data = '';
+  for await (const chunk of readable) {
+    data += chunk;
+  }
+  return data;
+}
+
+const getLatestReleaseTag = async (workspace) => {
+  const cmd = `git`;
+  const args = ['log', `--format=%D`, '--decorate-refs=tags'];
+  const result = await run(cmd, args, workspace);
+  const m = [...result.matchAll(/tag:\s([^\s,]+)/gm)];
+  if (m.length === 0) {
+    return '';
+  }
+  return m[0][1];
+};
+
+const getCommits = async (workspace, format, mode) => {
+  const cmd = `git`;
+  const args = ['log', `--format={{filter:%B}}${format}//`];
+  const result = await run(cmd, args, workspace);
+
+  return result.split('//\n').filter((s) => {
+    switch (mode) {
+      case modes.onlyPullRequests:
+        return s.match(/#\d+/) !== null;
+
+      case modes.onlyMergeCommit:
+        return s.match(/Merge/) !== null;
+
+      default:
+        return true;
+    }
+  }).map((s) => {
+    return s.replace(/{{filter:.+?}}/gs, '');
+  }).join('\n');
+};
+
+const main = async () => {
+  const inputs = parseInputs();
+
+  setOutput(
+    'latest-release',
+    await getLatestReleaseTag(inputs.workspace),
+  );
+  setOutput(
+    'commits',
+    await getCommits(inputs.workspace, inputs.format, inputs.mode),
+  );
+};
+
+try {
+  await main();
+} catch (error) {
+  setFailed(error.message);
+}
